29e487ea8e87a8e1181558fae8517030be237cd3
tien07011 <tien07011@gmail.com>
2025-12-11 14:20:51 +0700

    2.0.0

--- src/layouts/Toolbar/FakeAPI.tsx
+++ src/layouts/Toolbar/FakeAPI.tsx
@@ -3,9 +3,11 @@ import { TooltipButton } from "../../components/TooltipButton";
 import { createPathsFromSVG } from "../../utils/Shapes";
 import { getIcons } from "../../apis/icon";
 import { KonvaIcon } from "../../types/Konva";
-import { ApiTwoTone, CasinoTwoTone } from "@mui/icons-material";
+import { ApiTwoTone, CasinoTwoTone, ImageTwoTone } from "@mui/icons-material";
+import { BASE_URL } from "../../configs";
 import { AxiosResponse } from "axios";
 import React from "react";
+import { useMapContext } from "../../contexts/MapContext";
 
 type FakeApiEvent = {
   key: string;
@@ -45,8 +47,9 @@ function pickRandomIcon(icons: KonvaIcon[]): KonvaIcon | undefined {
 }
 
 export const ToolbarFakeAPI = React.memo((): React.JSX.Element => {
-  const { addShapes } = useShapesContext();
+  const { addShapes, alignShape } = useShapesContext();
   const { updateSnackbarAlert } = useStageContext();
+  const { setTopRight, setBottomLeft } = useMapContext();
 
   const [icons, setIcons] = React.useState<KonvaIcon[] | null>(null);
   const [loading, setLoading] = React.useState<boolean>(false);
@@ -120,6 +123,56 @@ export const ToolbarFakeAPI = React.memo((): React.JSX.Element => {
     }
   }, [addShapes, ensureIcons, updateSnackbarAlert]);
 
+  const onClickImportTestImage = React.useCallback(async (): Promise<void> => {
+    try {
+      const trLon = 106 + Math.random();
+      const trLat = 22 + Math.random();
+      const blLon = 104 + Math.random();
+      const blLat = 20 + Math.random();
+
+      await addShapes(
+        [
+          {
+            type: "image",
+            imageURL: `${BASE_URL}/assets/images/test.png`,
+          },
+        ],
+        false,
+        false,
+        { x: 0, y: 0 }
+      );
+
+      // Align the newly added image to the stage top-left
+      alignShape(undefined, "left", "top");
+      setTopRight({ lat: trLat, lon: trLon });
+      setBottomLeft({ lat: blLat, lon: blLon });
+
+      // Also add one fake icon to illustrate sending map & ship
+      const list = icons && icons.length ? icons : await ensureIcons();
+      let shipIcon: KonvaIcon | undefined = list.find((i) => /ship|boat|vessel/i.test(i.name));
+      if (!shipIcon) shipIcon = pickRandomIcon(list);
+      if (shipIcon) {
+        await addShapes(
+          [
+            {
+              type: "path",
+              paths: createPathsFromSVG(shipIcon.content, 160, 160),
+            },
+          ],
+          false,
+          false,
+          { x: 140, y: 140 }
+        );
+      }
+      updateSnackbarAlert?.(
+        `Imported test.png | TR(${trLat.toFixed(5)}, ${trLon.toFixed(5)}) BL(${blLat.toFixed(5)}, ${blLon.toFixed(5)})`,
+        "success"
+      );
+    } catch (err) {
+      updateSnackbarAlert?.(`Fake import ảnh lỗi: ${err}`, "error");
+    }
+  }, [addShapes, alignShape, setTopRight, setBottomLeft, ensureIcons, icons, updateSnackbarAlert]);
+
   return (
     <>
       {/* <TooltipButton
@@ -132,6 +185,12 @@ export const ToolbarFakeAPI = React.memo((): React.JSX.Element => {
         icon={<CasinoTwoTone />}
         title={"Random Icon"}
         onClick={onClickRandom}
+        disabled={loading}
+      />
+      <TooltipButton
+        icon={<ImageTwoTone />}
+        title={"Fake Import test.png"}
+        onClick={onClickImportTestImage}
         disabled={loading}
       />
     </>
